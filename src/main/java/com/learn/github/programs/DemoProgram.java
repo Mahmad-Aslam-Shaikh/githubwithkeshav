2b5afdfd6b82b17529d9a8890e1d9353d284b4ba
Aslam Shaikh <aslam.afzalpur759@gmail.com>
2023-11-03 22:33:41 +0530

    changed the commit

--- src/main/java/com/learn/github/programs/DemoProgram.java
+++ src/main/java/com/learn/github/programs/DemoProgram.java
@@ -4,6 +4,6 @@ public class DemoProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello Spidey");
-        System.out.println("Hello Peter");
+        System.out.println("Hello Gwen");
     }
 }
